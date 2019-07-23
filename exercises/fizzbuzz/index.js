e36d5ad3f85fc3ecb091dab476dfe47d361e63a1
Caridad Rivera <ccrive10@gmail.com>
2019-07-23 10:38:12 -0400

    donee

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,19 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  // this means that is a multiple of 3 because there are no remainders.
+for (let i = 1; i <= n; i++) {
+  if (i % 3 === 0 && i % 5 === 0)  {
+     console.log("fizz")
+  } else if (i % 5 === 0) {
+    console.log("buzz")
+  } else if (i % 3 === 0) {
+    console.log("fizzbuzz")
+  } else {
+    console.log(i)
+  }
+  }
+}
 
 module.exports = fizzBuzz;
