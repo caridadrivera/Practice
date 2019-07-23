8808a7e0804b617839a797fc933b91df7e81bae8
Caridad Rivera <ccrive10@gmail.com>
2019-07-23 12:54:57 -0400

    eeh

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,26 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //we are taking one big array and splitting it into small subarrays
+  // console.log(array,"im an array")
+  // console.log(size, "I'm the size")
+  // this is the array that holds all the different chuncks of data from the original array
+  const chuncked = [] ;
+
+  for(let element of array) {
+    // get the last elemet inside of the chuncked array
+    const last = chuncked[chuncked.length - 1]
+    console.log("im last", last)
+    console.log("im a size", size, chuncked)
+    if (!last || last.length === size) {
+  //divide the array into many subarrays, where each subarray is of length size
+      chuncked.push([element])
+    } else {
+      last.push(element)
+    }
+  }
+  return chuncked
+}
 
 module.exports = chunk;
