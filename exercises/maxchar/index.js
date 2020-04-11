caab087cfce6d37578eef18d20c701e5941e4840
Caridad Crystal Rivera <caridadrivera@users.noreply.github.com>
2020-04-11 19:55:28 -0400

    done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -1,5 +1,24 @@
 const maxChar = function(str) {
+ const chars = {}
+ let max = 0
+ let maxChar =""
 
-console.log(str)
-
+ for(let char of str){
+  if(!chars[char]){
+    chars[char] = 1
+  } else {
+    chars[char] ++
+  }
+ 
 }
+
+ for(let char in chars){
+  if(chars[char] > max){
+    max = chars[char]
+    maxChar= char
+  }
+}
+ return maxChar
+}
+
+maxChar("aaaamor")
